3ea6707aa8b4d19c800a88879046099c828eea5f
TranHoangNhu <31412677+TranHoangNhu@users.noreply.github.com>
2022-08-20 22:04:08 +0700

    d-none mobile

--- Js-Buoi-7/baitap.js
+++ Js-Buoi-7/baitap.js
@@ -57,7 +57,7 @@ function sortIncrease() {
 function checkPrime(n) {
   if (n < 2) return !1;
   for (var r = 2; r <= Math.sqrt(n); r++) if (n % r == 0) return !1;
-  return !0;
+  return !0; 
 }
 function findPrime() {
   for (var n = -1, r = 0; r < numArray.length; r++) {
